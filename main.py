95661eb2f388e01d40307231e221991be02e8607
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 13:40:58 -0400

    Auto-formatting code

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
+import pickle
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 import requests
 from bs4 import BeautifulSoup
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import nltk
-nltk.download('punkt') # Move nltk download statements before importing nltk modules
+# Move nltk download statements before importing nltk modules
+nltk.download('punkt')
 nltk.download('stopwords')
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import pandas as pd
-import matplotlib.pyplot as plt
-import os
-import pickle
+
 
 class WebScraper:
     def __init__(self, url):
@@ -23,6 +25,7 @@ class WebScraper:
         response = requests.get(self.url)
         soup = BeautifulSoup(response.text, 'html.parser')
         return soup.get_text()
+
 
 class GoogleDriveAPI:
     SCOPES = ['https://www.googleapis.com/auth/drive']
@@ -37,7 +40,8 @@ class GoogleDriveAPI:
                 creds = pickle.load(token)
 
         if not creds or not creds.valid:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', self.SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', self.SCOPES)
             creds = flow.run_local_server(port=0)
 
             with open('token.pickle', 'wb') as token:
@@ -54,12 +58,14 @@ class GoogleDriveAPI:
         ).execute()
         return results.get('files', [])
 
+
 class NLPProcessor:
     def preprocess_text(self, text):
         tokens = word_tokenize(text.lower())
         stop_words = set(stopwords.words('english'))
         tokens = [token for token in tokens if token not in stop_words]
         return tokens
+
 
 class User:
     def __init__(self, name):
@@ -69,18 +75,21 @@ class User:
     def read_article(self, article):
         self.read_articles.append(article)
 
+
 class Article:
     def __init__(self, title, text, author):
         self.title = title
         self.text = text
         self.author = author
 
+
 class RecommendationEngine:
     def __init__(self, articles):
         self.articles = articles
 
     def recommend_articles(self, user):
-        user_articles = [article for article in self.articles if article.author == user.name]
+        user_articles = [
+            article for article in self.articles if article.author == user.name]
 
         if not user_articles:
             return self.articles[0]
@@ -95,6 +104,7 @@ class RecommendationEngine:
             index = similarities[0].tolist().index(max_similarity)
             return self.articles[index]
 
+
 class AnalyticsCollector:
     def __init__(self):
         self.read_count = 0
@@ -105,17 +115,21 @@ class AnalyticsCollector:
     def get_read_count(self):
         return self.read_count
 
+
 def generate_visualization(data):
     df = pd.DataFrame(data)
     df.plot(kind='bar', x='x', y='y', color='blue')
     plt.show()
 
+
 def external_integration():
     # Code for external integration here (e.g. API calls)
     pass
 
+
 def main():
-    scraper = WebScraper('https://example.com') # Replace 'https://example.com' with the actual URL you want to scrape
+    # Replace 'https://example.com' with the actual URL you want to scrape
+    scraper = WebScraper('https://example.com')
     content = scraper.scrape_content()
 
     google_api = GoogleDriveAPI()
@@ -144,6 +158,7 @@ def main():
     generate_visualization(data)
 
     external_integration()
+
 
 if __name__ == '__main__':
     main()
